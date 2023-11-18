8e93f399e4f4b0bd2543543fd3413efab8e0c525
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2023-11-18 02:08:45 +0000

    Add remote install option to install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -21,13 +21,14 @@ usage() {
   fi
 
   echo -e ""
-  echo -e "Usage:\t$SCRIPT_NAME [-h] [-p PATH_TO_PIER] [-s SHIP_NAME]"
+  echo -e "Usage:\t$SCRIPT_NAME [-h] [-p PATH_TO_PIER] [-r PATH_TO_PIER] [-s SHIP_NAME]"
   echo -e ""
   echo -e "Install app files to a desk in an Urbit pier"
   echo -e ""
   echo -e "Options:"
   echo -e "  -h\tPrint script usage info"
-  echo -e "  -p\tPath to directory containing the pier"
+  echo -e "  -p\tPath to local directory containing the pier"
+  echo -e "  -r\tPath to remote directory containing the pier, access via SSH"
   echo -e "  -s\tName of pier to install to"
   echo -e ""
   exit $1
@@ -37,6 +38,7 @@ usage() {
 # Variables
 # --------------------------------------
 
+INSTALL_VIA_SSH=false
 SCRIPT_NAME=$(basename $0 | cut -d '.' -f 1)
 SCRIPT_DIR=$(dirname $0)
 ROOT_DIR=$(dirname $SCRIPT_DIR)
@@ -47,7 +49,7 @@ DESK_DIR="$ROOT_DIR/build/desk"
 # --------------------------------------
 
 # Parse arguments
-OPTS=":hp:s:"
+OPTS=":hp:r:s:"
 while getopts ${OPTS} opt; do
   case ${opt} in
     h)
@@ -55,6 +57,10 @@ while getopts ${OPTS} opt; do
       ;;
     p)
       PIER_PATH=$OPTARG
+      ;;
+    r)
+      PIER_PATH=$OPTARG
+      INSTALL_VIA_SSH=true
       ;;
     s)
       PIER=$OPTARG
@@ -74,5 +80,9 @@ done
 INSTALL_DIR="$PIER_PATH/$PIER/blog"
 
 echo "Attempting to install in $INSTALL_DIR"
-cp -r ${DESK_DIR}/* ${INSTALL_DIR}/
+if [[ $INSTALL_VIA_SSH == true ]]; then
+  scp -r ${DESK_DIR}/* ${INSTALL_DIR}/
+else
+  cp -r ${DESK_DIR}/* ${INSTALL_DIR}/
+fi
 echo "Successfully installed in $INSTALL_DIR"
