5cddaa5896970f47515af95f31c4712b65c47a3a
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2023-11-26 21:46:10 +0000

    Fix scries.ts

--- src/frontend/src/urbit/scries.ts
+++ src/frontend/src/urbit/scries.ts
@@ -1,7 +1,7 @@
 import Urbit from '@urbit/http-api'
 
 // generic scry function
-export async function scryUrbit (app: string, path: string) {
+export async function scryUrbit (app: string, path: string): Promise<any> {
   const urbit = new Urbit('')
   return await urbit.scry({ app, path })
 }
