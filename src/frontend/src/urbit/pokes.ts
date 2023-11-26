29fc7ad3ad69be021a94b6e2cb4c54863e4cfe95
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2023-11-26 22:05:29 +0000

    Fix pokes.ts

--- src/frontend/src/urbit/pokes.ts
+++ src/frontend/src/urbit/pokes.ts
@@ -5,23 +5,10 @@ import { type BlogAction } from '../types/blog'
 function emptyFunction (): void {}
 
 // generic poke functions
-export function pokeBlog (urbit: Urbit, action: BlogAction, onError?: () => void, onSuccess?: () => void) {
+export function pokeBlog (urbit: Urbit, action: BlogAction, onError?: () => void, onSuccess?: () => void): void {
   const pokeInput = {
     app: 'blog',
     mark: 'blog-action',
-    json: action,
-    onError: (typeof onError !== 'undefined') ? onError : emptyFunction,
-    onSuccess: (typeof onSuccess !== 'undefined') ? onSuccess : emptyFunction
-  }
-
-  urbit.poke(pokeInput)
-}
-
-// TODO could we narrow down action type from any?
-export function pokeUrbit (urbit: Urbit, app: string, mark: string, action: any, onError?: () => void, onSuccess?: () => void) {
-  const pokeInput = {
-    app,
-    mark,
     json: action,
     onError: (typeof onError !== 'undefined') ? onError : emptyFunction,
     onSuccess: (typeof onSuccess !== 'undefined') ? onSuccess : emptyFunction
