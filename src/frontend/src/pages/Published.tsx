1716fca899eb7b3112fa732e22a1b802ec1428bf
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2023-11-26 22:26:25 +0000

    Fix Published.tsx

--- src/frontend/src/pages/Published.tsx
+++ src/frontend/src/pages/Published.tsx
@@ -1,18 +1,18 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useMatch } from 'react-router-dom'
 import { api } from '../state/api'
 import Editor from '../components/Editor'
 import { useStore } from '../state/base'
 
 // published post
-export default function Published () {
+export default function Published (): JSX.Element {
   const match = useMatch('*')
   const setMarkdown = useStore((state) => state.setMarkdown)
   const setActiveTheme = useStore((state) => state.setActiveTheme)
 
   useEffect(() => {
-    const getDraft = async () => {
-      if (!match) return
+    const getDraft = async (): Promise<void> => {
+      if (match === null) return
       const res = await api.scry({
         app: 'blog',
         path: '/md/' + match?.pathname.split('/').slice(2).join('/') // TODO ugly code
