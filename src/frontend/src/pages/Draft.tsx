761ef191b08940570ca7966b773cc0e3a011a3d2
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2023-11-26 22:29:06 +0000

    Fix Draft.tsx

--- src/frontend/src/pages/Draft.tsx
+++ src/frontend/src/pages/Draft.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useMatch } from 'react-router-dom'
 import { api } from '../state/api'
 import Editor from '../components/Editor'
 import { useStore } from '../state/base'
 
 // draft post
-export default function Draft () {
+export default function Draft (): JSX.Element {
   const match = useMatch('*')
   const setMarkdown = useStore((state) => state.setMarkdown)
 
   useEffect(() => {
-    const getDraft = async () => {
-      if (!match) return
+    const getDraft = async (): Promise<void> => {
+      if (match === null) return
       const res = await api.scry({
         app: 'blog',
         path: match.pathname
