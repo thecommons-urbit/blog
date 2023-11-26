141d6dd6ffa7c5bb9ac835d328ea91d36d2fbe81
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2023-11-26 22:07:54 +0000

    Add TODO to scries.ts

--- src/frontend/src/urbit/scries.ts
+++ src/frontend/src/urbit/scries.ts
@@ -1,6 +1,8 @@
 import Urbit from '@urbit/http-api'
 
 // generic scry function
+// TODO narrow down type from any?
+//        the plan is to add more specific scry helpers leveraging this one
 export async function scryUrbit (app: string, path: string): Promise<any> {
   const urbit = new Urbit('')
   return await urbit.scry({ app, path })
